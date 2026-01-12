40ad13058c8b761b38d9b42583b1157993bdd553
송우인 <sowintildeblue@gmail.com>
2026-01-12 11:45:45 +0900

    객체로 html 구분

---
+++ struct.py
@@ -0,0 +1,14 @@
+body = [
+    {
+        "tag_name": "h1",
+        "tag_content": "공미남",
+        "tag_style": {
+            "color": "blue"
+        }
+    },
+    {
+        "tag_name": "p",
+        "tag_content": "공미남은 잘생겼다.",
+        "tag_style": {}
+    }
+]
