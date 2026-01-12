500fcae844e8b301de587576c7b8b0e0cee26db0
송우인 <sowintildeblue@gmail.com>
2026-01-12 17:17:05 +0900

    html 클래스/분해

---
+++ test/struct.py
@@ -0,0 +1,12 @@
+practics= {
+    "left_angle_bracket":"<",
+    "right_angle_bracket": ">",
+    "tag_name": "공미남",
+    "close_tag_slash":"/",
+    "childern_elements": []
+    }
+
+
+
+
+print(f"{practics["left_angle_bracket"]}{practics['tag_name']}{practics['right_angle_bracket']} 나야 {practics['left_angle_bracket']}{practics['close_tag_slash']}{practics['tag_name']}{practics['right_angle_bracket']}")
